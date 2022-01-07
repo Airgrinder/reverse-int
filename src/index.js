c742b2b0e386452edba551cf40763df117f22ed2
Airgrinder <51jework@mail.com>
2022-01-07 05:51:44 +0300

    feat: write reverse func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Array.from(String(Math.abs(n)), Number).reverse().join('')
+    }
+    return Array.from(String(n), Number).reverse().join('')
 }
